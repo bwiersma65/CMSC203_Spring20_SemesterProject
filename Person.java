fa091c8beba209a60af0def978d3aed0620b713c
bwiersma65 <55202800+bwiersma65@users.noreply.github.com>
2020-02-13 10:18:54 -0500

    Add files via upload
    First uploaded draft of Document and accompanying Code

---
+++ Person.java
@@ -0,0 +1,26 @@
+// Task #1
+
+public class Person {
+
+	private String name;
+	private int age;
+	
+	// Task #4
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	public void setAge(int age)
+	{
+		this.age = age;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	public int getAge()
+	{
+		return age;
+	}
+}
---
+++ PersonDemo.java
@@ -0,0 +1,20 @@
+// Task #2
+
+public class PersonDemo {
+
+	public static void main(String[] args) {
+
+		Person a;
+		
+		a = new Person();
+		
+//		a.name = "Todd";
+		a.setName("Todd");
+//		a.age = 20;
+		a.setAge(18);
+		
+		// Task #3
+		System.out.println(a.getName());
+	}
+
+}
